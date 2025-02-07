54df229232b3065af61e26ef55cf9ce6ff620482
Manoel Lima <manoel.lima@manaus.am.gov.br>
2025-02-07 17:31:02 -0400

    feat: fragment and another State

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ export default function App() {
   // 2) a function to update this value. It's common to destructure the
   // array. We can only call hooks at the top level of the function
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
@@ -23,34 +24,42 @@ export default function App() {
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
 
-      <p className="message">
-        Step {1}: {messages[step - 1]}
-      </p>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#FFF" }}
-          // we cannot call the function direct
-          // we just pass the function value. We don't call it
-          // onClick={() => alert("Previous")}
-          onClick={() => handlePrevious()}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#FFF" }}
-          onClick={() => handleNext()}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <p className="message">
+            Step {1}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#FFF" }}
+              // we cannot call the function direct
+              // we just pass the function value. We don't call it
+              // onClick={() => alert("Previous")}
+              onClick={() => handlePrevious()}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#FFF" }}
+              onClick={() => handleNext()}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
