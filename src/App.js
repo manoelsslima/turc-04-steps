fdcc2365d66c0820042d75ea439794cbc7cf9fef
Manoel Lima <manoel.lima@manaus.am.gov.br>
2025-02-07 15:18:22 -0400

    feat: controlling the States using useState

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,22 +7,27 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  // hooks in React are prefixed with "use", so useState is a hook.
+  // the argument is the default value of the state variable
+  // returns an array with two items. 1) the default value of the stae
+  // 2) a function to update this value. It's common to destructure the
+  // array. We can only call hooks at the top level of the function
+  const [step, setStep] = useState(1);
 
   function handlePrevious() {
-    alert("Previous");
+    if (step > 1) setStep(step - 1);
   }
 
   function handleNext() {
-    alert("Next");
+    if (step < 3) setStep(step + 1);
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">
